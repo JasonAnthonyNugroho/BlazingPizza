c03a9a94f4cf2f5aa12068469c78b5ee6d87dac2
itsHikariii <hasonanthonynugroho88@gmail.com>
2024-09-17 13:00:42 +0700

    Add project files.

---
+++ BlazingPizza/Data/PizzaStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using System.Data.Common;
+
+namespace BlazingPizza.Data
+{
+    public class PizzaStoreContext : DbContext
+    {
+        public PizzaStoreContext(DbContextOptions option) : base(option) { 
+        
+        }
+
+        public DbSet<PizzaSpecial> Specials { get; set; }
+    }
+}
